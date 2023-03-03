2b30b98a5c7d3c55a3eb3989f2f540dd2fbda48a
ukrwebprom <ukrwebprom@gmail.com>
2023-03-03 19:11:23 +0200

    simplify

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,15 @@
-import { useState, useEffect } from 'react';
 import { Label } from 'components/Common/Common.styled';
 import PropTypes from "prop-types";
 
 
 export default function Filter({callback}){
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    callback(filter)
-  }, [callback, filter])
 
   return (
       <Label>
         Find contacts by name
         <input
           type="text"
-          value={filter}
-          onChange={e => setFilter(e.target.value)}
+          onChange={e => callback(e.target.value)}
         />
       </Label>
     );
